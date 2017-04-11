b3e012c0d2b90eebe46a8d428661569295652f3c
mbaridonn <mbaridonn@gmail.com>
2017-04-11 17:33:12 -0300

    Prueba commit, inicio sockets

--- Kernel/src/Kernel.c
+++ Kernel/src/Kernel.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
 int main(void) {
+
+	//Esto tiene que ir en Kernel o en otro proyecto aparte que se llame Socket?
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = httons(8080);
+
 	puts("Proceso Kernel");
 	return EXIT_SUCCESS;
 }
