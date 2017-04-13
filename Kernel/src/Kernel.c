df82e6f34ff0e861d0e01dcc1a389d9394202605
FranGonzalezL <fran96.a@gmail.com>
2017-04-13 02:09:23 -0300

    Código base

--- Kernel/src/Kernel.c
+++ Kernel/src/Kernel.c
@@ -7,7 +7,6 @@
 
 int main(void) {
 
-	//Esto tiene que ir en Kernel o en otro proyecto aparte que se llame Socket?
 	struct sockaddr_in direccionServidor;
 	direccionServidor.sin_family = AF_INET;
 	direccionServidor.sin_addr.s_addr = INADDR_ANY;
@@ -15,13 +14,42 @@ int main(void) {
 
 	int servidor = socket(AF_INET, SOCK_STREAM, 0);
 
+	int activado = 1;
+	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
 	if(bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0){
 		perror("Falló el bind");
 		return 1;
 	}
 
 	printf("Estoy escuchando");
-	listen(servidor,100);
+	listen(servidor,SOMAXCONN);
+
+	//---------------------------------------------------------------------------------
+
+	struct sockaddr_in direcciónCliente;
+	unsigned int tamañoDirección;
+	int cliente = accept(servidor, (void*) &direcciónCliente, &tamañoDirección);
+
+	printf("Recibí una conexión en %d!!\n", cliente);
+	send(cliente, "Hola NetCat!", 13, 0);
+	send(cliente, ":)\n", 4, 0);
+
+	//----------------------------------------------------------------------------------
+
+	char* buffer = malloc(1000);
+
+	while (1) {
+		int bytesRecibidos = recv(cliente, buffer, 1000, 0);
+		if (bytesRecibidos <= 0) {
+			perror("El chabón se desconectó o bla.");
+			return 1;
+		}
+
+		buffer[bytesRecibidos] = '\0';
+		printf("Me llegaron %d bytes con %s\n", bytesRecibidos, buffer);
+	}
+	free(buffer);
 
 	//puts("Proceso Kernel");
 	//return EXIT_SUCCESS;
