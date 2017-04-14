99d99583b23681290c27c168c0747471ed37c8cf
martingsalazar <martin.salazar20@gmail.com>
2017-04-13 22:42:10 -0300

    Abstracciones de esperarConexion, aceptarConexion y recibirMensaje generadas.

--- Kernel/src/Kernel.c
+++ Kernel/src/Kernel.c
@@ -6,40 +6,38 @@
 #include <netinet/in.h>
 #define LONGMAX 1000
 
-int main(void) {
+int esperarConexion(int *servidor, struct sockaddr_in *direccionServidor) {
 
-	struct sockaddr_in direccionServidor;
-	direccionServidor.sin_family = AF_INET;
-	direccionServidor.sin_addr.s_addr = INADDR_ANY;
-	direccionServidor.sin_port = htons(8080);
-
-	int servidor = socket(AF_INET, SOCK_STREAM, 0);
-
+	(*servidor) = socket(AF_INET, SOCK_STREAM, 0);
 	int activado = 1;
-	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+	setsockopt((*servidor), SOL_SOCKET, SO_REUSEADDR, &activado,
+			sizeof(activado));
 
-	if (bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor))
-			!= 0) {
-		perror("Falló el bind");
+	if (bind((*servidor), (void*) &(*direccionServidor),
+			sizeof((*direccionServidor))) != 0) {
+		perror("Falló el bind.");
 		return 1;
 	}
 
 	printf("Estoy escuchando");
-	listen(servidor, SOMAXCONN);
+	listen((*servidor), SOMAXCONN);
 
-	//---------------------------------------------------------------------------------
+	return 0;
+}
+
+void aceptarConexion(int *servidor, int *cliente) {
 
 	struct sockaddr_in direccionCliente;
 	unsigned int tamanioDireccion;
-	int cliente = accept(servidor, (void*) &direccionCliente,
+	(*cliente) = accept((*servidor), (void*) &direccionCliente,
 			&tamanioDireccion);
 
-	printf("Recibí una conexión en %d!!\n", cliente);
-	//----------------------------------------------------------------------------------
+	printf("Recibí una conexión en %d!!\n", (*cliente));
+}
 
-	char* buffer = malloc(LONGMAX);
+int recibirMensajeDe(int *cliente, char *buffer) {
 
-	int bytesRecibidos = recv(cliente, buffer, LONGMAX, 0);
+	int bytesRecibidos = recv((*cliente), buffer, LONGMAX, 0);
 	if (bytesRecibidos <= 0) {
 		perror("El chabón se desconectó o bla.");
 		return 1;
@@ -48,32 +46,44 @@ int main(void) {
 	buffer[bytesRecibidos] = '\0';
 	printf("Me llegaron %d bytes con %s\n", bytesRecibidos, buffer);
 
+	return 0;
+}
+
+int main(void) {
+
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(8080);
+
+	int servidor;
+	int cliente;
+	char* buffer = malloc(LONGMAX);
+
+	esperarConexion(&servidor, &direccionServidor);
+
+	aceptarConexion(&servidor, &cliente);
+
+	recibirMensajeDe(&cliente, buffer);
+
+	//---------------------------------------------------------------------------------
+
+	//----------------------------------------------------------------------------------
+
 	//Conexion con los otros modulos (Memoria, Cpu, y FileSystem)
 	close(servidor);
-	servidor = socket(AF_INET, SOCK_STREAM, 0);
 
-	activado = 1;
-	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
-	if (bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor))
-			!= 0) {
-		perror("Falló el bind");
-		return 1;
-	}
+	esperarConexion(&servidor, &direccionServidor);
 
-	printf("Estoy escuchando");
-	listen(servidor, SOMAXCONN);
+	aceptarConexion(&servidor, &cliente);
 
-	cliente = accept(servidor, (void*) &direccionCliente,&tamanioDireccion);
-
-	printf("Recibí una conexión en %d!!\n", cliente);
-
-	send(cliente,buffer,LONGMAX,0);
+	send(cliente, buffer, LONGMAX, 0);
 
 	free(buffer);
 
 	close(servidor);
 
-	//puts("Proceso Kernel");
-	//return EXIT_SUCCESS;
+//puts("Proceso Kernel");
+//return EXIT_SUCCESS;
 	return 0;
 }
