7817a33dcf1506db02660f1c803724b25b724f72
mbaridonn <mbaridonn@gmail.com>
2017-05-07 00:28:08 -0300

    Cree mandarArchivo() en Consola (comando para enviar cualquier archivo que halla)

--- Consola/src/Consola.c
+++ Consola/src/Consola.c
@@ -21,9 +21,10 @@ enum procesos {
 	kernel, cpu, consola, file_system, memoria
 };
 
-
-void msjConexionCon(char *s){
-	printf("\n-------------------------------------------\nEstoy conectado con %s\n-------------------------------------------\n",s);
+void msjConexionCon(char *s) {
+	printf(
+			"\n-------------------------------------------\nEstoy conectado con %s\n-------------------------------------------\n",
+			s);
 } //Despues la borramos, la dejo para que tire el mensaje de con quien se conecta en el handshake.
 
 void *reservarMemoria(int tamanioArchivo) {
@@ -40,15 +41,16 @@ void settearVariables(t_config *archivo_Modelo) {
 	//config->ipKernel = config_get_string_value(archivo_Modelo,"IP_KERNEL");
 	config->puerto = config_get_int_value(archivo_Modelo, "PUERTO_KERNEL");
 }
-void mostrarArchivoConfig(){
-	 FILE *f;
+void mostrarArchivoConfig() {
+	FILE *f;
 
-	 f=fopen(RUTAARCHIVO,"r");
-	 int c;
-	 printf("------------------------------------------\n");
-	 while ((c = fgetc (f)) != EOF) putchar(c);
-	 printf("\n");
-	 printf("------------------------------------------\n");
+	f = fopen(RUTAARCHIVO, "r");
+	int c;
+	printf("------------------------------------------\n");
+	while ((c = fgetc(f)) != EOF)
+		putchar(c);
+	printf("\n");
+	printf("------------------------------------------\n");
 
 }
 
@@ -64,13 +66,61 @@ void leerArchivo() {
 	printf("Leí el archivo y extraje el puerto: %d \n", config->puerto);
 }
 
+void mandarArchivo(int *cliente) {
+
+	char *lineaIngresada, *comando, *nombreArchivo;
+	FILE *archivo;
+
+	printf("Ingrese mandarArchivo + nombre de su archivo\n");
+	printf("Ejemplo: mandarArchivo prueba.txt\n\n");
+
+	lineaIngresada = malloc(100);
+
+	fgets(lineaIngresada, 100, stdin);
+
+	comando = strtok(lineaIngresada, " ");
+	nombreArchivo = strtok(NULL, "\n");
+
+	if (strcmp("mandarArchivo", comando) != 0) {
+		printf("El comando ingresado no existe");
+	} else {
+		archivo = fopen(nombreArchivo, "rb");
+		if (archivo == NULL) {
+			printf("No se pudo leer el archivo\n");
+			exit(-1);
+		}
+	}
+
+	fseek(archivo, 0, SEEK_END);
+	u_int32_t fsize = ftell(archivo);
+	fseek(archivo, 0, SEEK_SET);
+
+	char *buffer = malloc(fsize + 1);
+	fread(buffer, fsize, 1, archivo);
+	fclose(archivo);
+	buffer[fsize] = '\0';
+	if (send(*cliente, &fsize, sizeof(u_int32_t), 0) == -1) {
+		printf("Error enviando longitud del archivo\n");
+		exit(-1);
+	}
+	if (send(*cliente, buffer, fsize + 1, 0) == -1) {
+		printf("Error enviando archivo\n");
+		exit(-1);
+	}
+	printf("El archivo se envió correctamente\n");
+
+	free(lineaIngresada);
+	free(buffer);
+
+}
+
 int main(void) {
 
 	leerArchivo();
 
 	struct sockaddr_in direccionServidor;
 	direccionServidor.sin_family = AF_INET;
-	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");//En realidad se le debería pasar config->ipKernel
+	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1"); //En realidad se le debería pasar config->ipKernel
 	direccionServidor.sin_port = htons(config->puerto);
 
 	int cliente;
@@ -83,28 +133,7 @@ int main(void) {
 	case kernel:
 		msjConexionCon("Kernel");
 
-		FILE * archivo = fopen("prueba.txt", "rb");
-		if (archivo == NULL) {
-			printf("No se pudo leer el archivo\n");
-			return EXIT_FAILURE;
-		}
-		fseek(archivo, 0, SEEK_END);
-		u_int32_t fsize = ftell(archivo);
-		fseek(archivo, 0, SEEK_SET);
-
-		char *buffer = malloc(fsize + 1);
-		fread(buffer, fsize, 1, archivo);
-		fclose(archivo);
-		buffer[fsize] = '\0';
-		if (send(cliente, &fsize, sizeof(u_int32_t), 0) == -1) {
-			printf("Error enviando longitud del archivo\n");
-			return EXIT_FAILURE;
-		}
-		if (send(cliente, buffer, fsize+1, 0) == -1) {
-			printf("Error enviando archivo\n");
-			return EXIT_FAILURE;
-		}
-		printf("El archivo se envió correctamente\n");
+		mandarArchivo(&cliente);
 
 		break;
 
