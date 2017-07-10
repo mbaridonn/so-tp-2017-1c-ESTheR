f2ed7f578f23a1d71bd829428c4e512916c7a956
Nico Rainhart <nicospam24@gmail.com>
2017-07-10 17:36:20 -0300

    Agregados intentos de primitivas

--- CPU/src/lib/primitivasAnSISOP.c
+++ CPU/src/lib/primitivasAnSISOP.c
@@ -22,6 +22,28 @@ bool esArgumento(t_nombre_variable identificador_variable){
 	return isdigit(identificador_variable);
 }
 
+t_stack_entry* stack_entry_create_and_initialize(){
+	t_stack_entry* stackEntry = calloc(1, sizeof(t_stack_entry));
+	stackEntry->args = list_create();
+	stackEntry->vars = list_create();
+	stackEntry->ret_pos = -1;
+	stackEntry->ret_vars->offset = 0;
+	stackEntry->ret_vars->page_number = 0;
+	stackEntry->ret_vars->tamanio = 0;
+	return stackEntry;
+}
+
+/*t_puntero_instruccion busquedaEtiqueta(t_nombre_etiqueta etiqueta){          PENDIENTE!!
+	for (int i = 0; i < pcbAEjecutar->tags; ++i) {
+		if(strcmp(pcbAEjecutar->indice_etiquetas[i].name, etiqueta) == 0){
+			return pcbAEjecutar->indice_etiquetas[i].PC;
+		} else {
+			return -1;   ESTÁ BIEN ESTO??
+		}
+	}
+	return -1;
+}*/
+
 //PRIMITIVAS
 
 t_puntero definirVariable(t_nombre_variable var_nombre){
@@ -30,7 +52,7 @@ t_puntero definirVariable(t_nombre_variable var_nombre){
 	if((pcbAEjecutar->stackPointer+4) > (stackSize * tamPag)){
 		printf("StackOverflow. Se finaliza el proceso\n");
 		//huboStackOver = true; (!!)
-		return -1;
+		return -1;//DEBERÍA PRODUCIR UN ERROR
 	}
 
 	int pagina = pcbAEjecutar->stackPointer / tamPag;
@@ -38,14 +60,14 @@ t_puntero definirVariable(t_nombre_variable var_nombre){
 
 	t_stack_entry* lineaStack;
 	if(list_is_empty(pcbAEjecutar->indice_stack->elements)){
-		lineaStack = stack_entry_create()/*t_stack_create()*/;
+		lineaStack = stack_entry_create_and_initialize();
 		list_add(pcbAEjecutar->indice_stack->elements, lineaStack);
 	}else{
 		lineaStack = list_get(pcbAEjecutar->indice_stack->elements, list_size(pcbAEjecutar->indice_stack->elements) - 1);
 	}
 
 	if(!esArgumento(var_nombre)){ // Es una variable
-		printf("ANSISOP_definirVariable %c", var_nombre);
+		printf("ANSISOP_definirVariable %c \n", var_nombre);
 		t_var* nuevaVar = malloc(sizeof(t_var));
 		nuevaVar->var_id = var_nombre;
 		nuevaVar->page_number = pagina;
@@ -54,7 +76,7 @@ t_puntero definirVariable(t_nombre_variable var_nombre){
 		list_add(lineaStack->vars, nuevaVar);
 	}
 	else{ // Es un argumento.
-		printf("ANSISOP_definirVariable (argumento) %c", var_nombre);
+		printf("ANSISOP_definirVariable (argumento) %c \n", var_nombre);
 		t_arg* nuevoArg = malloc(sizeof(t_var));
 		nuevoArg->page_number = pagina;
 		nuevoArg->offset = offset;
@@ -62,71 +84,151 @@ t_puntero definirVariable(t_nombre_variable var_nombre){
 		list_add(lineaStack->args, nuevoArg);
 	}
 
-	int posAbsoluta = pcbActual->stackPointer;
-	pcbActual->stackPointer += TAM_VARIABLE;
-	printf("Posicion relativa de %c: %d %d %d", var_nombre, pagina, offset, TAM_VARIABLE);
-	printf("Posicion absoluta de %c: %i", var_nombre, posAbsoluta);
+	int posAbsoluta = pcbAEjecutar->stackPointer;
+	pcbAEjecutar->stackPointer += TAM_VARIABLE;
+	printf("Posicion relativa de %c: %d %d %d \n", var_nombre, pagina, offset, TAM_VARIABLE);
+	printf("Posicion absoluta de %c: %i \n", var_nombre, posAbsoluta);
 	return posAbsoluta;
 }
 
 t_puntero obtenerPosicionVariable(t_nombre_variable var_nombre){
-	printf("Obtener posicion de %c\n", var_nombre);
-	return 0;
+	printf("ANSISOP_obtenerPosicion %c \n", var_nombre);
+	if(list_is_empty(pcbAEjecutar->indice_stack->elements)){
+		printf("En indice de stack se encuentra vacio \n");
+		return EXIT_FAILURE;//DEBERÍA PRODUCIR UN ERROR
+	}
+
+	t_puntero posicionAbsoluta;
+	t_stack_entry* contexto = list_get(pcbAEjecutar->indice_stack->elements, list_size(pcbAEjecutar->indice_stack->elements) - 1);
+
+	if(!esArgumento(var_nombre)){ //Es una variable
+		t_var* var_local;
+		bool notFound = true;
+		int i;
+		for(i=0; i < list_size(contexto->vars); i++){
+			var_local = list_get(contexto->vars, i);
+			if(var_local->var_id == var_nombre){
+				notFound = false;
+				break;
+			}
+		}
+		if(notFound){
+			printf("No se encontro la variable %c en el stack \n", var_nombre);
+			return EXIT_FAILURE;//DEBERÍA PRODUCIR UN ERROR
+		}
+		else{
+			posicionAbsoluta = var_local->page_number * tamPag + var_local->offset;
+		}
+	} else { //Es un argumento
+		if((var_nombre-'0') > list_size(contexto->args)){
+			return EXIT_FAILURE;//DEBERÍA PRODUCIR UN ERROR
+		}else{
+			t_var* argumento = list_get(contexto->args, var_nombre-'0');
+			//EL ARGUMENTO X TIENE QUE ESTAR NECESARIAMENTE EN LA POSICIÓN X??
+			posicionAbsoluta = argumento->page_number * tamPag + argumento->offset;
+		}
+	}
+	printf("Posicion absoluta de %c: %d \n", var_nombre, posicionAbsoluta);
+	return posicionAbsoluta;
 }
 
 t_valor_variable dereferenciar(t_puntero direccion_variable){
-	printf("Dereferenciar %d y su valor es: %d\n", direccion_variable, 0);
-	return 0;
-}
-/*
-t_valor_variable dereferenciar(t_puntero direccion_variable) {
-   //if(status_check() != EXECUTING)
-    //   return ERROR;
-	printf("print1\n");
-    //Hacemos el request a la UMC con el codigo 2
-    t_list *pedidos = NULL;
-    construir_operaciones_lectura(&pedidos, direccion_variable);
-    printf("print2\n");
-    void * variable_buffer = calloc(1, ANSISOP_VAR_SIZE), *tmp_buffer = NULL;
-    printf("print3\n");
-    int variable_buffer_index = 0;
-    char response_status;
-    printf("print4\n");
-    logical_addr * current_address = NULL;
-    printf("print5\n");
-    while(list_size(pedidos) > 0) {
-    	printf("print6\n");
-        current_address  = list_remove(pedidos, 0);
-//      asprintf(&tmp_buffer, "3%04d%04d%04d", current_address->page_number,
-//                 current_address->offset, current_address->tamanio);
-        printf("print7\n");
-        int tmp_buffer_index = 0;
-        printf("print8\n");
-        char operation = PEDIDO_BYTES;
-        printf("print9\n");
-        serializar_data(&operation, sizeof(char), &tmp_buffer, &tmp_buffer_index);
-        serializar_data(&current_address->page_number, sizeof(int), &tmp_buffer, &tmp_buffer_index);
-        serializar_data(&current_address->offset, sizeof(int), &tmp_buffer, &tmp_buffer_index);
-        serializar_data(&current_address->tamanio, sizeof(int), &tmp_buffer, &tmp_buffer_index);
-        printf("print10\n");
-        //log_info(cpu_log, "sending request : op:%c (%d,%d,%d) for direccion variable %d",operation, current_address->page_number,
-        //         current_address->offset, current_address->tamanio, direccion_variable);
-		//        log_info(cpu_log, "sending request : %s for direccion variable %d", &tmp_buffer, direccion_variable);
-        printf("print11\n");
-        variable_buffer_index += current_address->tamanio;
-        printf("print12\n");
-    }
+	printf("ANSISOP_dereferenciar posicion: %d \n", direccion_variable);
 
-	free(tmp_buffer);
-	printf("print\n");
-    //log_info(cpu_log, "Valor variable obtained : %d", *(int*)variable_buffer);
-    //return (t_valor_variable) *(t_valor_variable*)variable_buffer;
-	printf("Dereferenciar %d y su valor es: %d\n", direccion_variable, CONTENIDO_VARIABLE);
-	return CONTENIDO_VARIABLE;
-}*/
+	int pagina = (direccion_variable / tamPag) + pcbAEjecutar->cant_paginas_de_codigo;
+	int offset = direccion_variable % tamPag;
+	int tamanio = TAM_VARIABLE;
+
+	t_valor_variable* ptrValor = (int*)conseguirDatosDeLaMemoria(pcbAEjecutar->id_proceso, pagina, offset, tamanio);
+		//conseguirDatosDeLaMemoria ESTÁ EN MEMORIA.C
+		//SE PUEDE CASTEAR UN ARRAY DE 4 CHARS A INT ASÍ??
+		//CAPAZ NO SE OBTENGA BIEN EL VALOR: VER ENDIANNESS
+	//DEBERÍA RECIBIR UNA CONFIRMACIÓN??
+	t_valor_variable valor = *ptrValor;
+	free(ptrValor);
+	printf("Se obtuvo el valor: %d \n", valor);
+	return valor;
+}
 
 void asignar(t_puntero direccion_variable, t_valor_variable valor){
+	printf("ANSISOP_asignar (valor: %d, direccion_variable: %d\n)", valor, direccion_variable);
+	int pagina, offset, tamanio;
+	pagina = (direccion_variable / tamPag) + pcbAEjecutar->cant_paginas_de_codigo;
+	offset = direccion_variable % tamPag;
+	tamanio = TAM_VARIABLE;
 
+	//Enviar mensaje a Memoria: escribirPagina(pcbAEjecutar->id_proceso, pagina, offset, tamanio, (char*)&valor);
+	//DEBERÍA RECIBIR UNA CONFIRMACIÓN??
+}
+
+void irAlLabel(t_nombre_etiqueta nombre_etiqueta){
+	printf("ANSISOP_irALabel %s\n", nombre_etiqueta);
+	t_puntero_instruccion numeroInstr = busquedaEtiqueta(nombre_etiqueta);//PENDIENTE
+	printf("Numero de instruccion: %d", numeroInstr);
+	if(numeroInstr == -1){
+		printf("No se encontro la etiqueta\n");
+		return;
+	}
+	pcbAEjecutar->program_counter = numeroInstr - 1; //ES NECESARIO EL -1 ??
+}
+
+void llamarSinRetorno(t_nombre_etiqueta etiqueta){
+	printf("ANSISOP_llamarSinRetorno %s\n", etiqueta);
+	t_stack_entry* nuevaLineaStack = stack_entry_create_and_initialize();
+	nuevaLineaStack->ret_pos = pcbAEjecutar->program_counter;
+	list_add(pcbAEjecutar->indice_stack->elements, nuevaLineaStack);
+	irAlLabel(etiqueta);
+}
+
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar){
+	printf("ANSISOP_llamarConRetorno (etiqueta: %s, retornar: %d)\n", etiqueta, donde_retornar);
+	t_stack_entry* nuevaLineaStackEjecucionActual;
+	nuevaLineaStackEjecucionActual = stack_entry_create_and_initialize();
+	nuevaLineaStackEjecucionActual->ret_vars->page_number = donde_retornar / tamPag;
+	nuevaLineaStackEjecucionActual->ret_vars->offset = donde_retornar % tamPag;
+	nuevaLineaStackEjecucionActual->ret_vars->tamanio = TAM_VARIABLE;
+	nuevaLineaStackEjecucionActual->ret_pos = pcbAEjecutar->program_counter;
+	list_add(pcbAEjecutar->indice_stack->elements, nuevaLineaStackEjecucionActual);
+	irAlLabel(etiqueta);
+}
+
+void finalizar(void){
+	printf("ANSISOP_finalizar\n");
+	//Obtengo contexto quitado de la lista y lo limpio.
+	t_stack_entry* contexto = list_remove(pcbAEjecutar->indice_stack->elements, list_size(pcbAEjecutar->indice_stack->elements) - 1);
+	int i;
+	if(contexto != NULL){
+		pcbAEjecutar->stackPointer -= TAM_VARIABLE * (list_size(contexto->args) + list_size(contexto->vars)); // Disminuyo stackPointer del pcb
+		if(pcbAEjecutar->stackPointer >= 0){
+			for(i=0; i<list_size(contexto->args); i++){ // Limpio lista de argumentos del contexto
+				free(list_remove(contexto->args,i));
+			}
+			for(i=0; i<list_size(contexto->vars); i++){
+				free(list_remove(contexto->vars, i));
+			}
+		}
+		list_destroy(contexto->args);
+		list_destroy(contexto->vars);
+	}
+	if(list_is_empty(pcbAEjecutar->indice_stack->elements)){
+		//finPrograma = true;
+		printf("Finalizó la ejecucion del programa\n");
+		//finalizarPor(OP_CPU_PROGRAM_END);
+	}else{
+		pcbAEjecutar->program_counter = contexto->ret_pos;
+	}
+	free(contexto);
+}
+
+void retornar(t_valor_variable var_retorno){
+	printf("ANSISOP_retornar\n");
+	// Tomo contexto actual:
+	t_stack_entry* registroActual = list_get(pcbAEjecutar->indice_stack->elements, list_size(pcbAEjecutar->indice_stack->elements) - 1
+			/*pcbActual->stack->elements_count -1 ES LO MISMO??*/);
+	// Calculo la dirección de retorno a partir de retVar:
+	t_puntero offset_absoluto = (registroActual->ret_vars->page_number * tamPag) + registroActual->ret_vars->offset;
+	asignar(offset_absoluto, var_retorno);
+	finalizar();
 }
 
 t_valor_variable obtenerValorCompartida(t_nombre_compartida var_compartida_nombre){
@@ -134,26 +236,6 @@ t_valor_variable obtenerValorCompartida(t_nombre_compartida var_compartida_nombr
 }
 
 t_valor_variable asignarValorCompartida(t_nombre_compartida var_compartida_nombre, t_valor_variable var_compartida_valor){
-
-}
-
-void irAlLabel(t_nombre_etiqueta nombre_etiqueta){
-
-}
-
-void llamarSinRetorno(t_nombre_etiqueta etiqueta){
-
-}
-
-void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar){
-
-}
-
-void finalizar(void){
-
-}
-
-void retornar(t_valor_variable var_retorno){
 
 }
 
@@ -198,53 +280,3 @@ void escribir(t_descriptor_archivo descriptor_archivo, void* informacion, t_valo
 void leer(t_descriptor_archivo descriptor_archivo, t_puntero informacion, t_valor_variable tamanio){
 
 }
-/*
-t_list *armarDireccionesLogicasList(t_intructions *original_instruction) {
-
-    t_intructions* actual_instruction = calloc(1, sizeof(t_intructions));
-    actual_instruction->start = original_instruction->start;
-    actual_instruction->offset = original_instruction->offset;
-
-    t_list *address_list = list_create();
-    logical_addr *addr = (logical_addr*) calloc(1,sizeof(logical_addr));
-    int actual_page = 0;
-    //log_info(cpu_log, "Obtaining logic addresses for start:%d offset:%d",actual_instruction->start, actual_instruction->offset );
-    //First calculation
-    addr->page_number =  (int) actual_instruction->start / 10 ;		//setup->PAGE_SIZE
-    actual_page = addr->page_number;
-    addr->offset = actual_instruction->start % 10 ;					//setup->PAGE_SIZE
-    if(actual_instruction->offset > 10  - addr->offset ) {			//setup->PAGE_SIZE
-        addr->tamanio = 10  - addr->offset;							//setup->PAGE_SIZE
-    } else {
-        addr->tamanio = actual_instruction->offset;
-    }
-    list_add(address_list, addr);
-    actual_instruction->offset = actual_instruction->offset - addr->tamanio;
-
-    //ojo que actual_instruction->offset es un size_t
-    //cuando se lo decremente por debajo de 0, asumira su valor maximo
-    while (actual_instruction->offset != 0) {
-        addr = (logical_addr*) calloc(1,sizeof(logical_addr));
-        addr->page_number = ++actual_page;
-        addr->offset = 0;
-        //addr->tamanio = setup->PAGE_SIZE - addr->offset;
-        if(actual_instruction->offset > 10  - addr->offset ) {		//setup->PAGE_SIZE
-            addr->tamanio = 10 - addr->offset;						//setup->PAGE_SIZE
-        } else {
-            addr->tamanio = actual_instruction->offset;
-        }
-        actual_instruction->offset = actual_instruction->offset - addr->tamanio;
-        list_add(address_list, addr);
-    }
-    free(actual_instruction);
-    return address_list;
-}
-
-void construir_operaciones_lectura(t_list **pedidos, t_puntero posicion_variable) {
-    t_intructions instruccion_a_buscar;
-    instruccion_a_buscar.start = (t_puntero_instruccion) posicion_variable;
-    instruccion_a_buscar.offset = (size_t) ANSISOP_VAR_SIZE;
-    *pedidos = armarDireccionesLogicasList(&instruccion_a_buscar);
-}
-
-*/
